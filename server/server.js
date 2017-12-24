ab3de68b2855c1f8e18d761e912c5b908972f6c2
iahmedani <imran.ahmedani@gmail.com>
2017-12-24 14:48:08 +0500

    Add Get todo by ID

--- server/server.js
+++ server/server.js
@@ -1,37 +1,55 @@
+// external dependencies
 var express = require('express');
+var {ObjectID} = require('mongodb');
 var bodyParser = require('body-parser');
-
-var {mongoose} = require('./db/mongoose');
+//internal modules
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
-
+var {mongoose} = require('./db/mongoose');
+// App
 var app = express();
-
+// App Middleware body-parser
 app.use(bodyParser.json());
-
-app.post('/todos',(req, res) => {
+//Post /todos
+app.post('/todos', (req, res) => {
   var todo = new Todo({
     text: req.body.text
-});
-
-app.get('/todos', (req, res) =>{
-  Todo.find().then((todos)=>{
-    res.send({todos})
+  });
+  todo.save().then((doc) => {
+    res.send(doc)
   }, (e) => {
     res.status(400).send(e);
   });
 });
-
-  todo.save().then((doc)=>{
-    res.send(doc)
-  }, (e)=> {
-    res.status(400).send(e)
+//Get /todos
+app.get('/todos', (req, res) => {
+  Todo.find().then((docs)=> {
+    res.send(docs);
+  }, (e) => {
+    res.status(400).send(e);
   });
-  
+});
+//Get Specific todos by ID
+app.get('/todos/:id', (req, res) => {
+  var id = req.params.id;
+  if(!ObjectID.isValid(id)){
+    return res.status(400).send();
+  }
+
+  Todo.findById(id).then((todo) => {
+    if(!todo){
+      return res.status(400).send();
+    }
+    
+    res.send({todo});
+  }).catch((e )=> {
+    res.status(400).send();
+  });
 });
 
+//App startup
 app.listen(3000, () => {
-  console.log('Started server at port 3000');
+  console.log('Server started at 3000');
 });
-
-module.exports={app};
+//Exporting app
+module.exports = {app};
