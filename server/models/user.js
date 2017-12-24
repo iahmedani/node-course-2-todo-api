3b4f95eae5edb376b70ec73a3350b99ec655ddf6
iahmedani <imran.ahmedani@gmail.com>
2017-12-24 08:43:13 +0500

    Add POST /todos reoute and refactor mongoose

---
+++ server/models/user.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+var User = mongoose.model('User',{
+  email :{
+    type: String,
+    trim: true,
+    required: true,
+    minlength: 1
+  }
+});
+
+module.exports = {User};
