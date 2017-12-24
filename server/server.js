ad9c15011a67d101145d58811a7d6ae50c3373bf
iahmedani <imran.ahmedani@gmail.com>
2017-12-24 16:50:42 +0500

    Add DELETE /todos/:id

--- server/server.js
+++ server/server.js
@@ -54,8 +54,26 @@ app.get('/todos/:id', (req, res) => {
   })
   // Send status 404 in any error
   .catch((e )=> {
-    res.status(404).send();
+    res.status(400).send();
   });
+});
+
+app.delete('/todos/:id', (req, res) => {
+  var id = req.params.id;
+  if(!ObjectID.isValid(id)){
+    return res.status(404).send();
+  }
+
+  Todo.findByIdAndRemove(id).then((todo) => {
+    if(!todo) {
+      return res.status(404).send();
+    }
+
+    res.status(200).send({todo});
+  }).catch((e) => {
+     return res.status(400).send();
+  });
+
 });
 
 //App startup
