fc9477a9fa4647765a1e0ba0bbab760a087d459c
iahmedani <imran.ahmedani@gmail.com>
2017-12-24 17:16:10 +0500

    Add test DELETE /todos/:id

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -95,3 +95,40 @@ describe ('GET /todos:id by id', ()=>{
       .end(done)
   });
 });
+
+describe('DELETE /todos/:id', () => {
+  it('should remove a todo', (done) => {
+      var id = todos[1]._id.toHexString();
+    request(app)
+      .delete(`/todos/${id}`)
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.todo._id).toBe(id);
+      })
+      .end((err, res) => {
+        if(err) {
+          return done(err);
+        }
+              
+        Todo.findById(id).then((todo) => {
+          expect(todo).toNotExist();
+          done();
+        }).catch((e) => done(e));
+      });
+  });
+  
+  it('should return 404 if todo not found', (done) => {
+    var new_id = new ObjectID();
+    request(app)
+      .delete(`/todos/${new_id}`)
+      .expect(404)
+      .end(done);
+  });
+
+  it('should return 404 if object id is invalid', (done) => {
+    request(app)
+      .delete('/todos/123')
+      .expect(404)
+      .end(done);
+  });
+});
