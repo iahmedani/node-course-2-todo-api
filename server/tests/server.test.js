661ea1df7d2543671131840220a1d2d01ae7fb18
iahmedani <imran.ahmedani@gmail.com>
2017-12-30 08:50:47 +0500

    Add tests for DELETE /users/me/token

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -246,3 +246,23 @@ describe('POST /users', () => {
       .end(done);
   });
 });
+
+describe('DELETE /users/me/token', () => {
+  it('should should auth token on logout', (done) => {
+    request(app)
+      .delete('/users/me/token')
+      .set('x-auth', users[0].tokens[0].token)
+      .expect(200)
+      .end((err, res)=> {
+        if(err) {
+          return done(err);
+        }
+
+        User.findById(users[0]._id).then((user) => {
+          expect(user.tokens.length).toBe(0);
+          done();
+        }).catch((e) => done(e));        
+      });
+  });
+  
+});
